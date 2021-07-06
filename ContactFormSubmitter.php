18f270d7403d6f59399961fc358a8ccd5ff298f5
Colin Hoolwerf <choolwerf@gmail.com>
2021-07-05 20:19:19 -0500

    adjusted contact form and margins

--- ContactFormSubmitter.php
+++ ContactFormSubmitter.php
@@ -2,10 +2,10 @@
 $name = $_POST['inputname'];
 $company = $_POST['inputcompany'];
 $email = $_POST['inputemail'];
+$subject = $_POST["subject"];
 $message = $_POST['message'];
-$formcontent=" From: $name \n Company: $company \n Email: $email \n Message: $message";
+$formcontent= "From: $name \n Company: $company \n Email: $email \n Subject: $subject \n Message: $message";
 $recipient = "colinhoolwerf@gmail.com";
-$subject = "subject";
-mail($recipient, $subject, $formcontent) or die("Error!");
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
 echo "Thanks for the message, I'll get back to you as soon as I can!";
 ?>
